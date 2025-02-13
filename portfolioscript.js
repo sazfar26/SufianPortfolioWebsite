ea4ee0003171732b99994b2aaa10e7cd16f313bf
sazfar26 <sufianazfar@gmail.com>
2025-02-13 00:35:14 -0500

    added 3 projects and 3 performances, changed some div sizes, added a website icon

--- portfolioscript.js
+++ portfolioscript.js
@@ -1,32 +1,5 @@
-window.onload = function() {
-    const text = document.getElementById("fadeText");
-    text.classList.add("visible");		
+//handles the fading in of the words 'Software' and 'Music Enthusiast' on the title page
+window.onload = function() {  
+    const text = document.getElementById("fadeText");  //finds the elements with ID fadeText
+    text.classList.add("visible");	//Makes them visible.
 };
-
-function isElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-function handleScroll() {
-    const elements = document.querySelectorAll('.content');
-    elements.forEach(el => {
-        if (isElementInViewport(el)) {
-            el.classList.add('visible');
-        }
-    });
-}
-
-function titleHover() {
-    const el = document.getElementById("titleHover");
-    el.classList.add(restore);
-}
-
-
-window.addEventListener('scroll', handleScroll);
-window.addEventListener('load', handleScroll);
